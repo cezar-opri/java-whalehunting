9f194cc05c6f27d316060267609bc2291f492b80
cezar.opri <cezar.opri@gmail.com>
2025-07-02 19:25:39 +0300

    Test new ssh key

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,5 +1,5 @@
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("My cat doesn't like when I code and not pay attention to her");
+        System.out.println("My cat doesn't like when I code and not pay attention to her!");
     }
 }
